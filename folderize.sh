71d1707ce4e40d9b5f0d06dd8eacd69a78c6628c
mchuahui <mchuahui@sfu.ca>
2019-10-29 14:38:34 -0700

    set for-loop limit to 1024 as advertized

--- folderize.sh
+++ folderize.sh
@@ -5,7 +5,7 @@
 # "mkdir -p" creates a directory with just a number according to index i
 # "mv" moves all files matching the search pattern into the folder that was created 
 
-for i in {1..256}; do
+for i in {1..1024}; do
     if ls *."$i".wav 1> /dev/null 2>&1; then
         echo "$i".wav exists, creating folder...
 	mkdir -p "$i" && mv *."$i".wav "$i"
